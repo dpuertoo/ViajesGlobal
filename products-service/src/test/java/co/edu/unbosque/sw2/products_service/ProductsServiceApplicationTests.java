8f57f5b69ebd21b9a6b2e0877da8906d387daf2e
Daniel Ramos <114884528+DARV0706@users.noreply.github.com>
2024-11-15 13:18:25 -0500

    Api Database

---
+++ products-service/src/test/java/co/edu/unbosque/sw2/products_service/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.sw2.products_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
